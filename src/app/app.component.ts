b85a33408184b8d9276f267b8fe3e38a2cf41c0e
Oscar René Ballesteros Plaza <oscarballesteros@MacBook-Pro-de-Oscar.local>
2018-05-10 16:41:12 -0300

    Introduccion a Angular 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'PlatziSquare';
+  a = 3;
+  b = 5;
+  listo = false;
+  nombre:string = '';
+
+  constructor(){
+    setTimeout(()=> {
+      this.listo = true;
+    }, 3000)
+  }
+  hacerAlgo(){
+    alert('Hiciste click');
+  }
 }
+
+
+/* Un componente es cada uno de los elementos mas pequeños que componen un modulo como por ejemplo, cada vista que se implementaráUn modulo */
+/* Aqui se inicia el componente. Solo requiere el nombre del selector, una template URL y si es necesario un archivo con los estilos css */
+
+/* Tipos de data Bindign:
+* String Interpolation {{}} TypeScript => HTML
+* Property Binding [] TypeScript <= HTML
+* Event Binding TypeScript <= HTML
+* Two way Data Binding [()] TypeScript <=> HTML */
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
 
+import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+
+// ngModule es donde se declaran los modulos que seran usados en nuestra app:
 @NgModule({
+  // Aqui se agregan los componentes:
   declarations: [
     AppComponent
   ],
+  // Se llama todos los modulos que vamos a trabajar:
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
+  // servicios que vamos a necesitar para la app:
   providers: [],
+  // finalmente en bootstrap indicamos con cual componente vamos a iniciar:
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+/* Los módulos son las piezas mas grandes en las que se pueden dividir una app, uno modulo contiene uno o varios componentes */
